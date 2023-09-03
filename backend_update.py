049b5fd710aabeef1544738e82db13ff1d9c6395
Boris <khorbor1997@mail.ru>
2023-09-03 22:22:27 +1000

    Update backend_update.py
    I created new separate functions for DRY principle for decorators @app.delete @app.put

--- backend_update.py
+++ backend_update.py
@@ -14,6 +14,8 @@ class User(BaseModel):
 
 DB = []
 
+def find_user_by_username(username, DB):
+    return next((user for user in DB if user.username == username), None)
 
 @app.post("/users")
 async def register_user(username: User):
@@ -36,20 +38,24 @@ async def get_user(username: str):
 @app.put("/users")
 async def change_age(username: str, new_age: int):
     """Change user age."""
-    user = next((user for user in DB if user.username == username), None)
-    if user is None:
-        raise HTTPException(404, f"User with username '{username}' does not exist.")
+    found_user = find_user_by_username(username, DB) 
 
-    user.age = new_age
-    return {"message": f"Age for user '{username}' updated successfully"}
+    if found_user is not None: 
+        user.age = new_age
+        return {"message": f"Age for user '{username}' updated successfully"}
+    else:
+        raise HTTPException(404, f"User with username '{username}' does not exist.") 
+    
 
 
 @app.delete("/users")
 async def delete_user(username):
     """Delete existing user."""
+    found_user = find_user_by_username(username, DB)
     user = next((user for user in DB if user.username == username), None)
-    if user is None:
+    if user is not None:
+        DB.remove(user)
+        return {"message": f"User '{username}' deleted successfully"}
+    else:    
         raise HTTPException(404, f"User with username '{username}' does not exist.")
 
-    DB.remove(user)
-    return {"message": f"User '{username}' deleted successfully"}
